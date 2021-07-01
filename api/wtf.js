435df0609d72bf918fb97257f28fc0304b7bfb09
samsisle <samko9522@gmail.com>
2021-07-01 00:49:38 -0700

    Add console.log test

--- api/wtf.js
+++ api/wtf.js
@@ -223,5 +223,6 @@ async function getSFGovPages(options) {
   const { data } = await fetch(`${FORMS_API_URL}/api/sfgov`).then((res) =>
     res.json()
   );
+  console.log('test')
   return (data && data.forms) || [];
 }
